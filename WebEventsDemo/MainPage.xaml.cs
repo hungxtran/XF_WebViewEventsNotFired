2d3ea6a431748cd64731acf965950412a8fa01dd
Hung X. Tran <68488508+hungxtran@users.noreply.github.com>
2020-11-14 20:53:21 +0700

    Starter Project Initiated

---
+++ WebEventsDemo/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WebEventsDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void webviewNavigating(object sender, WebNavigatingEventArgs e)
+        {
+            labelLoading.IsVisible = true;
+        }
+
+        void webviewNavigated(object sender, WebNavigatedEventArgs e)
+        {
+            labelLoading.IsVisible = false;
+        }
+    }
+}
